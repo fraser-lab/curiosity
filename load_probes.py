08c61ae4d47684b655f149b4e915a18a571b4906
Iris Young <irisdyoung@gmail.com>
2022-03-12 11:28:07 -0800

    enable posttranscriptional modification probe

--- load_probes.py
+++ load_probes.py
@@ -7,5 +7,8 @@ from ion_screen_probe import AmIAnIonML
 # not used: AmIAnIon (original), runs VERY SLOWLY on larger models
 probe_collection.append(AmIAnIonML)
 
+from ptxm_probe import IsModifiedNucleotide
+probe_collection.append(IsModifiedNucleotide)
+
 # from map_CC_probe import CryoMapModelCC, XrayMapModelCC, NeutronMapModelCC
 # probe_collection.extend([CryoMapModelCC, XrayMapModelCC, NeutronMapModelCC])
